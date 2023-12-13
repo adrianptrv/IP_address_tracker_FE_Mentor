3d5b0cf9f0ef11b84eab89ccd73532d35fdeef67
adrianptrv <adrian.prtv1@gmail.com>
2023-12-13 17:15:49 +0200

    new

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Setting the Default Map view
 var obj;
 var IPholder;
 var map = L.map('map', {
@@ -7,29 +8,50 @@ var map = L.map('map', {
   [-90, 180]
 ]
 })
-// Setting the Default Map view
 
+
+// Loading the map itself
 L.tileLayer('https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=GNJkuzyL6GpeIIxa1RaW', {
  attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
  noWrap: true,
 }).addTo(map);
-// Loading the map itself
 
 
+//Creating new marker color
+var blackIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+
+//Getting user IP through the API
 fetch('http://ip-api.com/json/')
 .then(res => res.json())
 .then(data => {
  obj = data;
 })
-//Getting user IP through the API
 
-setTimeout( function() {
-document.getElementById("InField").value = obj.query
-IPholder = obj.query
-setMap();}, 500 )
+
 //Transferring user IP to variables and initiating the map pinpoint fucntion
+setTimeout( function() {
+IPholder = obj.query;
+setMap();}, 500 )
 
 
+//Updating the elements content with the current IP info
+function updateText(){
+$(".info-fields div:nth-of-type(1) p").text(obj.query);
+$(".info-fields div:nth-of-type(2) p").text(obj.city + ", " + obj.countryCode + "\n" + obj.zip)
+$(".info-fields div:nth-of-type(3) p").text(obj.timezone);
+$(".info-fields div:nth-of-type(4) p").text(obj.isp);
+}
+
+
+//Getting the input IP and running it through the IP API, and then executing the map pinpoint fucntion
 function getip(){
 IPholder = document.getElementById("InField").value
 fetch('http://ip-api.com/json/' + IPholder)
@@ -40,27 +62,20 @@ fetch('http://ip-api.com/json/' + IPholder)
  setTimeout( function() {
  setMap();}, 500 )
 }
-//Getting the input IP and running it through the IP API, and then executing the map pinpoint fucntion
 
 
-var blackIcon = new L.Icon({
-    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
-  });
-//Creating new marker color
 
 
+//Map pinpoint function (Setting the exact location on the map with the latitude and longitude we get from the IP API. Then adding a marker and popup with the IP to the same location.) 
 function setMap(){
  map.setView([obj.lat, obj.lon], 14);
  var marker = L.marker([obj.lat, obj.lon], {icon: blackIcon}).addTo(map);
  marker.bindPopup("<b>" + IPholder + "</b><br>is located here.").openPopup();
+ updateText();
 }
-//Map pinpoint function (Setting the exact location on the map with the latitude and longitude we get from the IP API. Then adding a marker and popup with the IP to the same location.) 
 
+
+//Changing map texture feature
 function changeMap(mapSelected){
  var mapname = mapSelected.value
 
@@ -83,12 +98,13 @@ function changeMap(mapSelected){
  }).addTo(map);
  }
 }
-//Changing map texture
 
+
+//Make input text be submitted with Enter key
 document.getElementById("InField").addEventListener("keypress", function(event) {
 if (event.key === "Enter") {
   event.preventDefault();
   document.getElementById("submitField").click();
 }
 });
-//Make input text be submitted with Enter key
+
